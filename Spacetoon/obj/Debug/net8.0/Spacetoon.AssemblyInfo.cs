f83955c10e31282f82c61f457eec2906f7e4d8ab
User <bayanalhassoun96@gmail.com>
2025-09-04 07:13:07 -0700

    SC

--- Spacetoon/obj/Debug/net8.0/Spacetoon.AssemblyInfo.cs
+++ Spacetoon/obj/Debug/net8.0/Spacetoon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spacetoon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f678468564d32909d738cb655e1dee25b3b46ed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spacetoon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spacetoon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
