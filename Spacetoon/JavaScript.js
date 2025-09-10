fefcb44827d430aa3e1bb2f6a6616f6818b53db8
User <bayanalhassoun96@gmail.com>
2025-09-10 06:31:50 -0700

    SC

--- Spacetoon/JavaScript.js
+++ Spacetoon/JavaScript.js
@@ -1,188 +1,66 @@
-﻿console.log("Hello Trainees")
-console.log("Hi")
+﻿//let element = document.getElementById("p1")
+//console.log(element)
+//console.log(element.innerText)
+//element.innerText = "P1 ID changed"
+//console.log(element.innerText)
 
-//let name = "Bayan"
-//alert(`Hello ${name} :)`)
+//let element2 = document.getElementsByClassName("c1");
+//console.log(element2)
+//element2[1].innerHTML = "element 2 changed"
 
+//let element3 = document.getElementsByTagName("p");
+//console.log(element3)
+//element3[3].innerHTML = "paragraph #4"
 
-//let result = confirm("Are you happy?")// true
-//if (result == true) {
-//    alert("🤩")
-//}
-//else {
-//    alert("😥")
-//}
+//let c1= document.getElementsByClassName("c1")
+//let el2 = document.getElementById("p1");
+//let el3 = document.getElementById("p2");
 
+//el2.innerText = c1[0].innerText;
+//el3.innerText = c1[1].innerText;
 
-//let name = prompt("what's your name?")
-//alert(`Hello ${name}`);
+//let p = document.createElement("p"); // <p> </p>
+//let text = document.createTextNode("Paragraph 3") // Pargraph 3
+//p.appendChild(text) // <p> Paragraph 3 </p>
 
-//function Sum(num1, num2) {
-//    alert(num1+num2)
-//}
+//let parentDiv = document.getElementsByTagName("div"); // [div]
+///*parentDiv[0].appendChild(p)*/
+//let child = document.getElementById("p2");
+//parentDiv[0].insertBefore(p, child);
 
+let x1 = document.createElement("label"); // <label> </label>
 
-//Sum(10,20)
+let x2 = document.createTextNode("Email"); // Email
 
-/*var, let, const*/
-//var x = 1; //
-//x = "Name";
-//x = true;
-//x = 'a';
-
-//var x = 3;
-
-
-
-
-/*var global scope, function scope*/
-
-//var y = 1; //global scope
-//var y = 3;
-//function Print() {
-//    console.log(y)
-//}
-
-//Print();
-
-//var z = 3;
-//function Print2() {
-//    var z = 1;
-//    var z = 2;
-//    console.log(z); // 2
-//}
-//Print2();
-//console.log(z)
-
-//var z = 2;
-//if (true) {
-//    var z = 1;
-//}
-
-//console.log(z);
-
-//var s = 1;
-//while (true)
-//{
-//    var s = 5;
-//    break;
-//}
-
-//console.log(s)
-
-/*let Global, function, local scope*/
-
-//let y = 1; //global scope
-//let y = 2
-//function Print() {
-//    console.log(y)
-//}
-
-//Print();
-
-//let z = 2;
-//function Print2() {
-//    let z = 1;
-//}
-//Print2();
-//console.log(z)
-
-//let z = 5;
-//if (true) {
-//    let z = 1;
-//}
-
-//console.log(z);
-
-//let s = 10;
-//while (true) {
-//    let s = 5;
-//    break;
-//}
-
-//console.log(s)
-
-//var c = 5;
-//let d = 3;
-//function Print()
-//{
-//    var a = 1;
-//    let b = 3;
-//}
-
-//{
-//    let s = 10;
-//    var z = 4;
-//}
-
-//Print();
-//console.log(a); // no
-//console.log(b); // no
-//console.log(c); // Yes
-//console.log(d); //yes
-//console.log(s); //no
-//console.log(z); // yes
+x1.appendChild(x2); // <label> Email </label>
 
 
-//var i = 20;
+let y1 = document.createElement("label"); // <label> </label>
 
-//for (var i = 0; i < 10; i++) { // i = 10
-//    console.log(i) // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+let y2 = document.createTextNode("password"); // password
 
-//}
+y1.appendChild(y2); //<label> password </label>
 
-//console.log(i); // 10
 
-//let a = 50;
+let c1 = document.createElement("button"); // <button> </button>
 
-//for (let a = 0; a < 10; a++) { // i =10
-//    console.log(a) // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-//}
+let c2 = document.createTextNode("Login") // Login
 
-//console.log(a);
+c1.appendChild(c2); // <button> Login </button>
 
 
 
-//let x = 10;
-//var y = 20;
-//let z = 30
-//function Print() {
-//    let x = 1;
-//    var y = 2;
-//    let z = x+y
-//}
-
-//console.log(x); // 10
-//console.log(y); // 20
-//console.log(z); // 30
+let form = document.getElementsByTagName("form"); // [form]
+form[0].appendChild(c1);
 
 
-//let r;
-//r = 5;
-
-//const h;
-//h = 7;
-
-//const pi = 3.14;
+let inputs = document.getElementsByTagName("input"); // [input1, input2]
 
 
-//let names = ["bayan", "Ahmad", "Shaima", "Mohammad"]
+form[0].insertBefore(x1, inputs[0]);
 
-//for (var i in names) {// i = 1
-//    alert(names[i]);
-//}
+form[0].insertBefore(y1, inputs[1]);
 
-let person = {
-    id: 1,
-    name: "Shaima",
-    email: "Shaima@gmail.com",
-    age: 22
-}
 
-let car = {
-    color: "red",
-    model: 2020,
-    capacity: 5
-}
 
-console.log(car.color)
+
